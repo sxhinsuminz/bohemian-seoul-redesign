a08d6adf36831076446b1bed54c741dc34237ef4
sxhinsuminz <shineamt0@gmail.com>
2022-04-25 15:27:54 +0900

    20220425 html

---
+++ style.css
@@ -0,0 +1,43 @@
+body, ul, li {
+    margin:0;
+    padding:0;
+    list-style:none;
+  }
+  
+  a {
+    color:inherit;
+    text-decoration:none;
+  }
+  
+  .con {
+    margin-left:auto;
+    margin-right:auto;
+  }
+  
+  .body {
+    width : 1920px;
+    height : 3429px;
+    background-color : red;
+  }
+  
+  .body-con{
+    width: 1200px;
+    height: 3429px;
+    background-color : white;
+    margin: 0 auto ;
+    
+  }
+  .section {
+    width: 1920px;
+    height: 1000px;
+    background-color : #999;
+  }
+  .menu-bar {
+    width: 1920px;
+    height:64px;
+    background-color:#777;
+  }
+  .logo-box{
+    width: 358px;
+    height: 64px;
+  }
